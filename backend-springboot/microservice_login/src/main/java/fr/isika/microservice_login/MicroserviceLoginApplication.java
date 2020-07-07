9bd539a8c5ae962457cd5c768e51bb010447e20b
Alpha Niang <alpha.niang@hotmail.com>
2020-07-07 18:08:41 +0200

    ajout de post, put delete et mise en place de jwt

---
+++ backend-springboot/microservice_login/src/main/java/fr/isika/microservice_login/MicroserviceLoginApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.microservice_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceLoginApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/microservice_login/src/test/java/fr/isika/microservice_login/MicroserviceLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isika.microservice_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
